ac648fb492f20f26ad2d78b04006c809b5d024b9
Arhamsannz <107133293+Arhamsannz@users.noreply.github.com>
2026-09-16 00:15:16 +0700

    Create app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackStart(),
+  ],
+})
